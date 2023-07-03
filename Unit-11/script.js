96f7fa39fed23d1de60504ab20e4732fbb0c8327
Jared <jared_glenn@yahoo.com>
2023-07-03 17:30:07 -0600

    finish v1 of fruit search project

--- Unit-11/script.js
+++ Unit-11/script.js
@@ -4,28 +4,75 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
+const find = e => {
+	e.preventDefault();
+	console.log(input.value);
+}
+
 function search(str) {
-	let results = [];
-
-	// TODO
-
+	const results = fruit.filter(value => value.includes(str));
+	console.log(results)
+	
 	return results;
 }
 
 function searchHandler(e) {
-	e.preventDefault();
-	const searchTerm = input.value;
+	const results = search(input.value.toLowerCase());
+	console.log(input.value.toLowerCase());
+
+	if (input.value !== ""){
+		showSuggestions(results);
+	}
+	else {
+		showSuggestions([])
+	}
+	
 }
 
-function showSuggestions(results, inputVal) {
-
-	// TODO
+function showSuggestions(results) {
+	let count = 0;
+	while (suggestions.firstChild){
+		suggestions.removeChild(suggestions.firstChild);
+	}
+	for (result of results){
+		if (count > 4){
+			break;
+		}
+		else {
+			const suggestion = document.createElement("div");
+			suggestion.innerText = result;
+			suggestion.classList.add('suggestion');
+			suggestions.append(suggestion);
+			count += 1;
+		}
+		
+	}
 }
 
-function useSuggestion(e) {
-	// TODO
+const highlightSuggestion = e => {
+	if (e.target.classList.contains('suggestion')){
+		e.target.style.backgroundColor = "rgb(255, 72, 0)";
+		e.target.style.color = "white";
+	}
 }
+
+const unhighlightSuggestion = e => {
+	if (e.target.classList.contains('suggestion')){
+		e.target.style.backgroundColor = "transparent";
+		e.target.style.color = "black";
+	}
+}
+
+const useSuggestion = e => {
+	input.value = e.target.innerText;
+	while (suggestions.firstChild){
+		suggestions.removeChild(suggestions.firstChild);
+	}
+}
+
 
 input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
-fruitForm.addEventListener("submit", searchHandler);
+suggestions.addEventListener('mouseover', highlightSuggestion);
+suggestions.addEventListener('mouseout', unhighlightSuggestion);
+fruitForm.addEventListener("submit", find);
