63bac68ba7da14b8cef578185282fc8c5b704085
Jared <jared_glenn@yahoo.com>
2023-07-10 11:54:24 -0600

    Add bold on search suggestions. Remove console.logs. Add scrolling results.

--- Unit-11/script.js
+++ Unit-11/script.js
@@ -4,21 +4,21 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-const find = e => {
-	e.preventDefault();
-	console.log(input.value);
-}
-
 function search(str) {
 	const results = fruit.filter(value => value.toLowerCase().includes(str));
-	console.log(value.toLowerCase())
-	
+
+	for (let i = 0; i < results.length; i++) {
+		const stringLength = str.length;
+		const indexOfString = results[i].toLowerCase().indexOf(str);
+		results[i] = results[i].slice(0, indexOfString) + "<b>" +
+		 results[i].slice(indexOfString, (indexOfString+stringLength)) + "</b>" +
+		  results[i].slice((indexOfString+stringLength));
+	}
 	return results;
 }
 
-function searchHandler(e) {
+function searchHandler() {
 	const results = search(input.value.toLowerCase());
-	console.log(input.value.toLowerCase());
 
 	if (input.value !== ""){
 		showSuggestions(results);
@@ -30,22 +30,14 @@ function searchHandler(e) {
 }
 
 function showSuggestions(results) {
-	let count = 0;
 	while (suggestions.firstChild){
 		suggestions.removeChild(suggestions.firstChild);
 	}
 	for (result of results){
-		if (count > 4){
-			break;
-		}
-		else {
-			const suggestion = document.createElement("div");
-			suggestion.innerText = result;
-			suggestion.classList.add('suggestion');
-			suggestions.append(suggestion);
-			count += 1;
-		}
-		
+		const suggestion = document.createElement("div");
+		suggestion.innerHTML = result;
+		suggestion.classList.add('suggestion');
+		suggestions.append(suggestion);
 	}
 }
 
