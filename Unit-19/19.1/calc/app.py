db097ed98600ae3a86e3b3a0383d7b15f89ab166
Jared <jared_glenn@yahoo.com>
2023-07-30 11:55:51 -0600

    complete greet app

--- Unit-19/19.1/calc/app.py
+++ Unit-19/19.1/calc/app.py
@@ -1,1 +1,17 @@
-# Put your app in here.
+from flask import Flask, request
+import operations
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add(): 
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    res = operations.add(a, b)
+    return f"""
+<html>
+    <body>
+    <h1>{res}</h1>
+    </body>
+</html>
+"""
