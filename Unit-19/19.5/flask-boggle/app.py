555ac825077d2661ed9ffba8b369debb2d9c8565
Jared <jared_glenn@yahoo.com>
2023-08-08 17:23:49 -0600

    start project

---
+++ Unit-19/19.5/flask-boggle/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, session
+from flask_debugtoolbar import DebugToolbarExtension
+from boggle import Boggle
+
+boggle_game = Boggle()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
+
+
+
+@app.route('/')
+def home():
+    session['board'] = boggle_game.make_board()
+    return render_template('home.html', board=session['board'])
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
