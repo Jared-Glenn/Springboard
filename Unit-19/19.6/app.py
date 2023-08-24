1c227dde2b504a5e356d8e36ab16cc5e5f3d6b24
Jared <jared_glenn@yahoo.com>
2023-08-24 14:49:26 -0600

    add functionality to respond to non-appearing queries

--- Unit-19/19.6/app.py
+++ Unit-19/19.6/app.py
@@ -1,11 +1,10 @@
 import math
 
 def binary_search(arr, item):
-    found = False
     first_length = len(arr)
     top =  first_length - 1
     bottom = 0
-    while not found and top < first_length and bottom >= 0:
+    while top < first_length and bottom >= 0 and top >= bottom:
         mid = math.floor((top - bottom)/2) + bottom
         if arr[mid] == item:
             return mid
@@ -13,8 +12,7 @@ def binary_search(arr, item):
             bottom = mid + 1
         elif arr[mid] > item:
             top = mid - 1
-        else:
-            return "Error"
+    return f'{item} not found.'
 
 
-print(binary_search([1,2,3,4,5,6,7], 1))
+print(binary_search([1,2,3,4,5,6,7], 8))
