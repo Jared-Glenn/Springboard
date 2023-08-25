cf9cf76e7846576fb56b70613e6d6160ee59eb8e
Jared <jared_glenn@yahoo.com>
2023-08-25 00:24:02 -0600

    correction for using jest to test

--- Unit-19/19.6/prob-divide-conquer/count-zeroes.js
+++ Unit-19/19.6/prob-divide-conquer/count-zeroes.js
@@ -2,24 +2,16 @@
 
 function countZeroes(arr) {
     let top = arr.length - 1;
-    console.log(`top: ${top}`)
     let bottom = 0;
     while (top >= bottom) {
         let mid = Math.floor((top - bottom)/2) + bottom;
-        console.log(`mid: ${mid}`)
-        console.log(`arr[mid]: ${arr[mid]}`)
-        console.log(`top: ${top}`)
-        console.log(`bottom: ${bottom}`)
         if (arr[mid] === 1 && arr[mid+1] === 0){
-            console.log("Returning")
             return (arr.length - (mid + 1));
         }
         else if (arr[mid] === 1){
-            console.log(`is one`)
             bottom = mid + 1;
         }
         else if (arr[mid] === 0){
-            console.log(`is zero`)
             top = mid - 1;
         }
     }
