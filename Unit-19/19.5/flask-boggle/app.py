3a56251681cb0f0f09925bcd01da55b2bec46308
Jared <jared_glenn@yahoo.com>
2023-08-30 13:47:22 -0600

    finish basic flask boggle

--- Unit-19/19.5/flask-boggle/app.py
+++ Unit-19/19.5/flask-boggle/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
@@ -8,20 +8,50 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 
 debug = DebugToolbarExtension(app)
 
+word_list = []
+
 
 
 @app.route('/')
 def home():
+    """View the main page and start a new game."""
+    global word_list
+    word_list = []
+    if not session.get('high_score'):
+        session['high_score'] = 0
+    if not session.get('games'):
+        session['games'] = 0
     session['board'] = boggle_game.make_board()
-    return render_template('home.html', board=session['board'])
+    return render_template('home.html', board=session['board'], high_score=session['high_score'], games=session['games'])
 
 
 @app.route('/guess')
 def guess():
+    """Check a guess against the current board."""
     word_guess = request.args.get("word_guess")
-    board = session['board']
-    result = boggle_game.check_valid_word(board, word_guess)
-    return result
+    if word_guess not in word_list:
+        board = session['board']
+        result = boggle_game.check_valid_word(board, word_guess)
+        
+        if result == "ok":
+            word_list.append(word_guess)
+        return result
+    else:
+        return "not-on-board"
+
+@app.route('/score')
+def score():
+    """Takes the final score for the current game, compares it to the high
+    score, and displays the new high score. Iterates the current number of games
+    in the session.
+    """
+    final_score = int(request.args.get("final_score"))
+    if final_score > session.get('high_score'):
+        session['high_score'] = final_score
+    session['games'] += 1
+    info = {"games": session.get('games'), "high_score": session.get('high_score')}
+    info = jsonify(info)
+    return info
 
 
 if __name__ == '__main__':
--- Unit-19/19.5/flask-boggle/static/scripts.js
+++ Unit-19/19.5/flask-boggle/static/scripts.js
@@ -2,20 +2,53 @@ const $wordGuessForm = $('#word-guess-form');
 const $correctList = $('.correct-list');
 const $notOnBoardList = $('.not-on-board-list');
 const $notWordList = $('.not-word-list');
+const $score = $('.score');
+const $timer = $('.timer');
+const $highScore = $('.high-score');
+const $games = $('.games')
 
 const submitGuess = (evt) => {
     evt.preventDefault();
-    const $wordGuess = $("#word-guess").val();
-    axios.get(`http://127.0.0.1:5000/guess?word_guess=${$wordGuess}`)
-  .then(function (response) {
-    // handle success
-    let res = response['data']
-    
-    if (res === "ok") {
-      $correctList.append(`<li>${$wordGuess}</li>`)
+    if (running === true){
+      const $wordGuess = $("#word-guess").val();
+      axios.get(`http://127.0.0.1:5000/guess?word_guess=${$wordGuess}`)
+    .then(function (response) {
+      // handle success
+      let res = response['data']
+      
+      if (res === "ok") {
+        $correctList.append(`<li>${$wordGuess}</li>`)
+        let score = parseInt($score.text());
+        score += $wordGuess.length;
+        $score.text(score.toString());
+      }
+      else if (res === "not-on-board") {
+        $notOnBoardList.append(`<li>${$wordGuess}</li>`)
+      }
+      else if (res === "not-word") {
+        $notWordList.append(`<li>${$wordGuess}</li>`)
+      }
+    })
     }
-  })
-    console.log($wordGuess)
+    $('#word-guess').val("")
 }
 
 $wordGuessForm.on('submit', submitGuess)
+
+let i = 60;
+let running = true;
+
+let timerInterval = setInterval(function () {
+  if (i <= 0){
+    clearInterval(timerInterval);
+    running = false;
+    console.log($score.text())
+    axios.get(`http://127.0.0.1:5000/score?final_score=${$score.text()}`)
+  .then(function (response) {
+    $games.text(response['data']['games'])
+    $highScore.text(response['data']['high_score'])
+  })
+  }
+  $timer.text(i);
+  i--;
+}, 1000);
