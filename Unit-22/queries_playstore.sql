7ca172d31a1c7303d94edcd587a9ec3ee038290c
Jared <jared_glenn@yahoo.com>
2023-09-07 13:07:07 -0600

    finished playstore questions

--- Unit-22/queries_playstore.sql
+++ Unit-22/queries_playstore.sql
@@ -16,3 +16,31 @@ SELECT app_name, reviews FROM analytics ORDER BY reviews DESC LIMIT 5;
 SELECT app_name FROM analytics WHERE rating >= 4.8 ORDER BY reviews DESC LIMIT 1;
 
 -- 6
+SELECT category, AVG(rating) FROM analytics GROUP BY category ORDER BY AVG(rating) DESC;
+
+-- 7
+SELECT app_name, price, rating FROM analytics WHERE rating < 3 ORDER BY price DESC LIMIT 1;
+
+-- 8
+SELECT app_name, rating, min_installs FROM analytics WHERE min_installs <= 50 AND rating >= 0 ORDER BY rating DESC;
+
+-- 9
+SELECT app_name, rating, reviews FROM analytics WHERE rating < 3 AND reviews >= 10000;
+
+-- 10
+SELECT app_name, price, reviews FROM analytics WHERE price BETWEEN 0.1 AND 1 ORDER BY reviews DESC LIMIT 10;
+
+-- 11
+SELECT app_name, last_updated FROM analytics ORDER BY last_updated ASC;
+
+-- 12
+SELECT app_name, price FROM analytics ORDER BY price DESC LIMIT 1;
+
+-- 13
+SELECT SUM(reviews) FROM analytics;
+
+-- 14
+SELECT category, COUNT(category) FROM analytics GROUP BY category HAVING COUNT(category) > 300;
+
+-- 15
+SELECT app_name, reviews, min_installs, (min_installs/reviews) AS proportion FROM analytics WHERE min_installs >= 100000 ORDER BY (min_installs/reviews) DESC LIMIT 1;
