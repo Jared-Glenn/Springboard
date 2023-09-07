0abe5126c68118a405f99ea8c67a4e2810ea0545
Jared <jared_glenn@yahoo.com>
2023-09-07 01:16:08 -0600

    started on playstore questions

--- Unit-22/queries_playstore.sql
+++ Unit-22/queries_playstore.sql
@@ -1,1 +1,18 @@
 -- Comments in SQL Start with dash-dash --
+
+-- 1
+SELECT app_name FROM analytics WHERE id = 1880;
+
+-- 2
+SELECT id, app_name FROM analytics WHERE last_updated = '2018-08-01';
+
+-- 3
+SELECT category, COUNT(category) FROM analytics GROUP BY category;
+
+-- 4
+SELECT app_name, reviews FROM analytics ORDER BY reviews DESC LIMIT 5;
+
+-- 5
+SELECT app_name FROM analytics WHERE rating >= 4.8 ORDER BY reviews DESC LIMIT 1;
+
+-- 6
