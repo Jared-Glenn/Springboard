7e1afab79ac370d5a7916ed078d0e0844c6a722b
Jared <jared_glenn@yahoo.com>
2023-09-12 14:01:53 -0600

    complete outer_space critique

--- Unit-22/22.4/outer_space.sql
+++ Unit-22/22.4/outer_space.sql
@@ -7,22 +7,61 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
+CREATE TABLE galaxies
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
 CREATE TABLE planets
 (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
   orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  galaxy INTEGER REFERENCES galaxies
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  planetid INTEGER REFERENCES planets
+);
+
+INSERT INTO galaxies
+  (id, name)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  (1, 'Milky Way'),
+  (2, 'Andromeda');
+
+INSERT INTO planets
+  (id, name, orbital_period_in_years, orbits_around, galaxy)
+VALUES
+  (1, 'Earth', 1.00, 'The Sun', 1),
+  (2, 'Mars', 1.88, 'The Sun', 1),
+  (3, 'Venus', 0.62, 'The Sun', 1),
+  (4, 'Neptune', 164.8, 'The Sun', 1),
+  (5, 'Proxima Centauri b', 0.03, 'Proxima Centauri', 1),
+  (6, 'Gliese 876 b', 0.23, 'Gliese 876', 1);
+
+INSERT INTO moons
+  (id, name, planetid)
+VALUES
+  (1, 'The Moon', 1),
+  (2, 'Phobos', 2),
+  (3, 'Deimos', 2),
+  (4, 'Naiad', 4),
+  (5, 'Thalassa', 4),
+  (6, 'Despina', 4),
+  (7, 'Galatea', 4),
+  (8, 'Larissa', 4),
+  (9, 'S/2004 N 1', 4),
+  (10, 'Proteus', 4),
+  (11, 'Triton', 4),
+  (12, 'Nereid', 4),
+  (13, 'Halimede', 4),
+  (14, 'Sao', 4),
+  (15, 'Laomedeia', 4),
+  (16, 'Neso', 4);
+
