d818b73c2926dddf0dd8666bf9a276cea96be8bb
Jared <jared_glenn@yahoo.com>
2023-09-19 00:08:14 -0600

    add edit and delete routes

--- Unit-23/23.1/app.py
+++ Unit-23/23.1/app.py
@@ -35,12 +35,78 @@ def users():
     
     return render_template("index.html", users=users)
 
+# New Users
 
 @app.route('/users/new')
-def add_user():
+def new_user():
     """Form for adding new users."""
     
-    return render_template("add_user.html")
+    return render_template("new_user.html")
+
+
+@app.route('/users/new', methods=["POST"])
+def add_user():
+    """Add the new user and redirect to the user's page."""
+    
+    first_name = request.form["first-name"]
+    last_name = request.form["last-name"]
+    image_url = request.form["image-url"]
+    
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return redirect(f"/users/{new_user.id}")
+
+# User Info
+
+@app.route('/users/<user_id>')
+def user_page(user_id):
+    """Page template for each individual user."""
+    
+    user = User.query.get(user_id)
+    
+    return render_template("user.html", user=user)
+
+# Edit User
+
+@app.route('/users/<user_id>/edit', methods=["GET"])
+def edit_user(user_id):
+    """Edit an individual user."""
+    
+    user = User.query.get(user_id)
+    
+    return render_template("edit_user.html", user=user)
+
+
+@app.route('/users/<user_id>/edit', methods=["POST"])
+def apply_edit(user_id):
+    """Edit an individual user."""
+    
+    print("!!!!!!!!!!!!!!!!!HELLOOOOOOO!!!!!!!!!!!!!!!!!!!!!")
+    
+    user_to_edit = User.query.get_or_404(user_id)
+    
+    user_to_edit.first_name = request.form["first-name"]
+    user_to_edit.last_name = request.form["last-name"]
+    user_to_edit.image_url = request.form["image-url"]
+    
+    db.session.commit()
+    
+    return redirect(f"/users/{user_id}")
+
+# Delete User
+
+@app.route('/users/<user_id>/delete')
+def delete_page(user_id):
+    """Page template for each individual user."""
+    
+    user_to_delete = User.query.get_or_404(user_id)
+    
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    
+    return redirect("/users")
 
 
 if __name__ == '__main__':
